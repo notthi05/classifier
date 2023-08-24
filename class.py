ec376906c675984b120a383faf88e712beabe9fb
Your Name <notsuchi020@gmail.com>
2023-08-24 11:37:34 +0900

    add result

--- class.py
+++ class.py
@@ -1,14 +1,17 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, TensorDataset
 from transformers import BertTokenizer, BertForSequenceClassification, AdamW
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report, roc_auc_score
 
-df = pd.read_csv('data.csv')
+# データの読み込み
+df = pd.read_csv('data.csv', encoding='UTF-8')
 
-# データの前処理（小文字化）
+# テキストデータを小文字化
 df['text'] = df['text'].str.lower()
 
+# 入力データとラベルの抽出
 texts = df['text'].values
 labels = df['label'].values
 
@@ -18,17 +21,21 @@ train_texts, test_texts, train_labels, test_labels = train_test_split(texts, lab
 # BERTのトークナイザーの準備
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-# BERTに入力する形式にデータを変換
-train_encodings = tokenizer(train_texts.tolist(), truncation=True, padding=True, max_length=128)
-test_encodings = tokenizer(test_texts.tolist(), truncation=True, padding=True, max_length=128)
+# データのトークン化とエンコーディング
+train_encodings = tokenizer(list(train_texts), truncation=True, padding=True, max_length=128)
+test_encodings = tokenizer(list(test_texts), truncation=True, padding=True, max_length=128)
+
+# 入力データと注意マスクのテンソル化
+train_inputs = torch.tensor(train_encodings['input_ids'])
+train_masks = torch.tensor(train_encodings['attention_mask'])
+train_labels = torch.tensor(train_labels)
+test_inputs = torch.tensor(test_encodings['input_ids'])
+test_masks = torch.tensor(test_encodings['attention_mask'])
+test_labels = torch.tensor(test_labels)
 
 # データセットの作成
-train_dataset = torch.utils.data.TensorDataset(torch.tensor(train_encodings['input_ids']),
-                                               torch.tensor(train_encodings['attention_mask']),
-                                               torch.tensor(train_labels))
-test_dataset = torch.utils.data.TensorDataset(torch.tensor(test_encodings['input_ids']),
-                                              torch.tensor(test_encodings['attention_mask']),
-                                              torch.tensor(test_labels))
+train_dataset = TensorDataset(train_inputs, train_masks, train_labels)
+test_dataset = TensorDataset(test_inputs, test_masks, test_labels)
 
 # バッチサイズの設定
 batch_size = 32
@@ -38,13 +45,14 @@ train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True
 test_dataloader = DataLoader(test_dataset, batch_size=batch_size)
 
 # BERTモデルの準備
-model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2)
+model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=3)
 
 # GPUが利用可能な場合は、モデルをGPUに移動
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
 
-# 最適化手法と学習率の設定
+# 損失関数と最適化手法の設定
+criterion = torch.nn.CrossEntropyLoss()
 optimizer = AdamW(model.parameters(), lr=3e-5)
 
 # 最大エポック数の設定
@@ -53,35 +61,49 @@ num_epochs = 10
 # 学習ループ
 for epoch in range(num_epochs):
     model.train()
+    total_loss = 0
 
     for batch in train_dataloader:
         batch = tuple(t.to(device) for t in batch)
         inputs, masks, labels = batch
 
         optimizer.zero_grad()
-
         outputs = model(inputs, attention_mask=masks, labels=labels)
         loss = outputs.loss
         loss.backward()
         optimizer.step()
+        total_loss += loss.item()
 
-    # テストデータでの評価
-    model.eval()
+    avg_loss = total_loss / len(train_dataloader)
+    print(f'Epoch {epoch+1}/{num_epochs} - Avg Loss: {avg_loss:.4f}')
 
-    correct = 0
-    total = 0
+# テストデータでの予測と評価
+model.eval()
+all_predictions = []
+all_labels = []
 
-    with torch.no_grad():
-        for batch in test_dataloader:
-            batch = tuple(t.to(device) for t in batch)
-            inputs, masks, labels = batch
+with torch.no_grad():
+    for batch in test_dataloader:
+        batch = tuple(t.to(device) for t in batch)
+        inputs, masks, labels = batch
 
-            outputs = model(inputs, attention_mask=masks)
-            logits = outputs.logits
-            _, predicted = torch.max(logits, dim=1)
+        outputs = model(inputs, attention_mask=masks)
+        logits = outputs.logits
+        predicted = torch.argmax(logits, dim=1).cpu().numpy()
 
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+        all_predictions.extend(predicted)
+        all_labels.extend(labels.cpu().numpy())
 
-    accuracy = correct / total
-    print(f'Epoch {epoch+1}/{num_epochs} - Accuracy: {accuracy:.4f}')
+# 混同行列
+confusion = confusion_matrix(all_labels, all_predictions)
+print("Confusion Matrix:")
+print(confusion)
+
+# Precision, Recall, F1-score
+report = classification_report(all_labels, all_predictions, target_names=['Class 0', 'Class 1', 'Class 2'])
+print("Classification Report:")
+print(report)
+
+# AUC
+auc = roc_auc_score(all_labels, all_predictions, multi_class='ovr', average='macro')
+print(f"AUC: {auc:.4f}")
